c0629e3baf50772ba9b87bf15e16828a8e24b928
apanic <496043045@qq.com>
2023-10-03 19:03:13 +0800

    is

--- src/utils/is.js
+++ src/utils/is.js
@@ -1,9 +1,81 @@
-// import {isArray} from "ant-design-vue/es/_util/util";
+const toString = Object.prototype.toString;
 
-// export function isEmpty(val) {
-//     if (isArray)
-// }
 
+export function is(val , type) {
+    return toString.call(val) === `[object ${type}]`;
+}
+
+export function isString(val) {
+    return is(val, 'String');
+}
+
+export function isArray(val) {
+    return val && Array.isArray(val);
+}
+
+export function isEmpty(val) {
+    if (isArray(val) || isString(val)) {
+        return val.length === 0;
+    }
+
+    if (val instanceof Map || val instanceof Set) {
+        return val.size === 0;
+    }
+
+    if (isObject(val)) {
+        return Object.keys(val).length === 0;
+    }
+
+    return false;
+}
+
+export function isDef(val) {
+    return typeof val !== 'undefined';
+}
+
+export function isUnDef(val) {
+    return !isDef(val);
+}
+
+export function isObject(val) {
+    return val !== null && is(val, 'Object');
+}
+
+export function isDate(val) {
+    return is(val,'Date');
+}
+
+export function isNull(val) {
+    return val === null;
+}
+
+export function isNullAndUnDef(val) {
+    return isUnDef(val) && isNull(val);
+}
+
+export function isNumber(val) {
+    return is(val, 'Number');
+}
+
+export function isBoolean(val) {
+    return is(val, 'Boolean');
+}
+
+export function isFunction(val) {
+    return typeof val === 'function';
+}
+
+export function isWindow(val) {
+    return typeof window !== 'undefined' && is(val,'window')
+}
+
+export function isElement(val) {
+    return isObject(val) && !!val.tagName;
+}
+
+export function isMap(val) {
+    return is(val,'Map')
+}
 
 export function isUrl(path) {
     const reg =
