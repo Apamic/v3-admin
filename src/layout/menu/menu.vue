2abc2a6081ed6ed105c00e7f104fb99f58674964
apanic <496043045@qq.com>
2023-11-11 21:36:13 +0800

    add editconfig

--- src/layout/menu/menu.vue
+++ src/layout/menu/menu.vue
@@ -1,116 +1,111 @@
 <template>
-  <a-layout-sider :collapsed="collapsed" :trigger="null" collapsible>
-    <div class="logo" />
-    <a-menu v-model:selectedKeys="state.selectedKeys" theme="dark" mode="inline">
+    <a-layout-sider :collapsed="collapsed" :trigger="null" collapsible>
+        <div class="logo"/>
+        <a-menu v-model:selectedKeys="state.selectedKeys" theme="dark" mode="inline">
+            <template v-for="(item,k) in routerList">
+                <menuItem :item="item">
+                </menuItem>
 
-      <template v-for="(item,k) in routerList" >
+                <!--        <a-sub-menu :key="i.name" v-if="i.children?.length > 0">-->
+                <!--          <template #title>-->
+                <!--              <span>-->
+                <!--                <span>{{i.name}}</span>-->
+                <!--              </span>-->
+                <!--          </template>-->
+                <!--          <a-menu-item :key="i.name" @click="clickMenuItem(i.name)">-->
+                <!--            <span >{{i.name}}</span>-->
+                <!--          </a-menu-item>-->
+                <!--        </a-sub-menu>-->
+                <!--        <a-menu-item :key="i.name" v-else @click="clickMenuItem(i.name)">-->
+                <!--          <span >{{i.name}}</span>-->
+                <!--        </a-menu-item>-->
+            </template>
 
-        <menuItem :item="item">
-
-        </menuItem>
-
-<!--        <a-sub-menu :key="i.name" v-if="i.children?.length > 0">-->
-<!--          <template #title>-->
-<!--              <span>-->
-<!--                <span>{{i.name}}</span>-->
-<!--              </span>-->
-<!--          </template>-->
-<!--          <a-menu-item :key="i.name" @click="clickMenuItem(i.name)">-->
-<!--            <span >{{i.name}}</span>-->
-<!--          </a-menu-item>-->
-<!--        </a-sub-menu>-->
-<!--        <a-menu-item :key="i.name" v-else @click="clickMenuItem(i.name)">-->
-<!--          <span >{{i.name}}</span>-->
-<!--        </a-menu-item>-->
-      </template>
-
-<!--      <a-menu-item key="1" @click="clickMenuItem('option1')">-->
-<!--        <span v-bind="$attrs">Options 1</span>-->
-<!--      </a-menu-item>-->
-<!--      <a-sub-menu key="sub2">-->
-<!--        <template #title>-->
-<!--            <span>-->
-<!--              <span>Team</span>-->
-<!--            </span>-->
-<!--        </template>-->
-<!--        <a-menu-item key="team1" @click="clickMenuItem('team1')">-->
-<!--          Team 1-->
-<!--        </a-menu-item>-->
-<!--        <a-sub-menu key="team2">-->
-<!--          <template #title>-->
-<!--            <span>-->
-<!--              <span>Team2</span>-->
-<!--            </span>-->
-<!--          </template>-->
-<!--          <a-menu-item key="team22"  @click="clickMenuItem('team22')">-->
-<!--            <span>-->
-<!--              <TeamOutlined/>-->
-<!--              <span>Team22</span>-->
-<!--            </span>-->
-<!--          </a-menu-item>-->
-<!--        </a-sub-menu>-->
-<!--      </a-sub-menu>-->
-    </a-menu>
-  </a-layout-sider>
+            <!--      <a-menu-item key="1" @click="clickMenuItem('option1')">-->
+            <!--        <span v-bind="$attrs">Options 1</span>-->
+            <!--      </a-menu-item>-->
+            <!--      <a-sub-menu key="sub2">-->
+            <!--        <template #title>-->
+            <!--            <span>-->
+            <!--              <span>Team</span>-->
+            <!--            </span>-->
+            <!--        </template>-->
+            <!--        <a-menu-item key="team1" @click="clickMenuItem('team1')">-->
+            <!--          Team 1-->
+            <!--        </a-menu-item>-->
+            <!--        <a-sub-menu key="team2">-->
+            <!--          <template #title>-->
+            <!--            <span>-->
+            <!--              <span>Team2</span>-->
+            <!--            </span>-->
+            <!--          </template>-->
+            <!--          <a-menu-item key="team22"  @click="clickMenuItem('team22')">-->
+            <!--            <span>-->
+            <!--              <TeamOutlined/>-->
+            <!--              <span>Team22</span>-->
+            <!--            </span>-->
+            <!--          </a-menu-item>-->
+            <!--        </a-sub-menu>-->
+            <!--      </a-sub-menu>-->
+        </a-menu>
+    </a-layout-sider>
 </template>
 
 <script setup>
-  //import { Menu } from 'ant-design-vue';
-  import {reactive, ref} from "vue";
-  import menuItem from './menuItem.vue'
-  // import {
-  //   UserOutlined,
-  //   PieChartOutlined,
-  //   TeamOutlined,
-  //   FileOutlined,
-  //   DesktopOutlined,
-  // } from '@ant-design/icons-vue';
-  import {useRoute, useRouter} from "vue-router";
+//import { Menu } from 'ant-design-vue';
+import {reactive, ref} from "vue";
+import menuItem from './menuItem.vue'
+// import {
+//   UserOutlined,
+//   PieChartOutlined,
+//   TeamOutlined,
+//   FileOutlined,
+//   DesktopOutlined,
+// } from '@ant-design/icons-vue';
+import {useRoute, useRouter} from "vue-router";
 
 
-
-  defineProps({
+defineProps({
     collapsed: {
-      type: Boolean
+        type: Boolean
     }
-  });
+});
 
-  const currentRoute = useRoute();
-  const router = useRouter();
+const currentRoute = useRoute();
+const router = useRouter();
 
-  const routerList = router.options.routes[0].children
-  console.log(routerList,'routerList')
+const routerList = router.options.routes[0].children
+console.log(routerList, 'routerList')
 
 
-
-  const state = reactive({
+const state = reactive({
     selectedKeys: ['option1'],
     openKeys: [],
     routerList: routerList
-  })
+})
 
 
-  // console.log(currentRoute.meta)
+// console.log(currentRoute.meta)
 
-  const clickMenuItem = ( key ) => {
+const clickMenuItem = (key) => {
     console.log(key)
     if (key === currentRoute.name) return;
     const targetRoute = getRouteByName(key);
-    const { isExt, openMode } = targetRoute?.meta || {};
+    const {isExt, openMode} = targetRoute?.meta || {};
     if (isExt && openMode !== 2) {
-      window.open(key);
+        window.open(key);
     } else {
-      router.push({ name: key });
+        router.push({name: key});
     }
-  };
+};
 
 
 </script>
 
 <style scoped>
 .logo {
-  height: 32px;
-  background: rgba(255, 255, 255, 0.3);
-  margin: 16px;
+    height: 32px;
+    background: rgba(255, 255, 255, 0.3);
+    margin: 16px;
 }
 </style>
--- src/layout/menu/menuItem.vue
+++ src/layout/menu/menuItem.vue
@@ -1,49 +1,48 @@
 <template>
-  <a-sub-menu :key="item.name" v-if="item.meta?.type === 0">
-    <template #title>
-      <span>
-        <span>{{item.name}}</span>
-      </span>
-    </template>
-    <template v-for="(items,k) in item.children" v-if="item.children.length > 0">
-      <menuItem :item="items"></menuItem>
-    </template>
-  </a-sub-menu>
-  <a-menu-item :key="item.name" v-else @click="clickMenuItem(item.name)">
-    <span >{{item.name}}</span>
-  </a-menu-item>
+    <a-sub-menu :key="item.name" v-if="item.meta?.type === 0">
+        <template #title>
+            <span>
+               <span>{{ item.name }}</span>
+            </span>
+        </template>
+        <template v-for="(items,k) in item.children" v-if="item.children.length > 0">
+            <menuItem :item="items"></menuItem>
+        </template>
+    </a-sub-menu>
+    <a-menu-item :key="item.name" v-else @click="clickMenuItem(item.name)">
+        <span>{{ item.name }}</span>
+    </a-menu-item>
 </template>
 
 <script setup>
 import {useRoute, useRouter} from "vue-router";
 
 defineOptions({
-  name: 'menuItem'
+    name: 'menuItem'
 })
 
 defineProps({
-  item: {
-    type: Object,
-    default: () => {}
-  }
+    item: {
+        type: Object,
+        default: () => ({})
+    }
 })
-
 
 const router = useRouter();
 const currentRoute = useRoute();
 
 const getRouteByName = name => router.getRoutes().find((n) => n.name === name);
 
-const clickMenuItem = ( key ) => {
-  console.log(key)
-  if (key === currentRoute.name) return;
-  const targetRoute = getRouteByName(key);
-  const { isExt, openMode } = targetRoute?.meta || {};
-  if (isExt && openMode !== 2) {
-    window.open(key);
-  } else {
-    router.push({ name: key });
-  }
+const clickMenuItem = (key) => {
+
+    if (key === currentRoute.name) return;
+    const targetRoute = getRouteByName(key);
+    const {isExt, openMode} = targetRoute?.meta || {};
+    if (isExt && openMode !== 2) {
+        window.open(key);
+    } else {
+        router.push({name: key});
+    }
 };
 
 </script>
